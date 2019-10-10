f4a26e820951c1e66865834a7cea87f187259060
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-10 13:30:10 -0700

    Strings

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,33 +6,40 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-     Robot bot =new Robot(); 
+	Robot rob = new Robot();
 	void go() {
-		bot.setPenWidth(5);
-	    bot.penDown();
+		
 		// 4. Make the robot move as fast as possible
-bot.setSpeed(100);
+          rob.setSpeed(200);
 		// 5. Set the pen width to 5
-
+             rob.penDown();
+             rob.setPenWidth(5); 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
-	bot.setRandomPenColor();
+             for (int i = 0; i < 4; i++) {
+            	 // 7. Set the pen color to random
+	             
 			// 1. Call the drawSquare() method
-	drawSquare();
+	           drawSquare();
+	           
+	
 			// 8. Turn the robot 90 degrees to the right
+				rob.turn(90);
+			}
+			
 	      
 	}
 	
 	
-	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+	
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		for(int i=0; i<4 ; i++) { 
-			 bot.move(200);	
-		      bot.turn(90);	
-		      }
+		void drawSquare() {
+			JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+			for(int i=0; i<4 ; i++) { 
+           	 rob.setRandomPenColor();
+           	 rob.move(200);
+           	 rob.turn(90);
+            }
+			 
 	}        
 
 	public static void main(String[] args) {
