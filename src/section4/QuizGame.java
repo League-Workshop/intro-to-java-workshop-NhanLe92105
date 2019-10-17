05b8c88718c5e71f6541b53b70bda09c9f181cbc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-17 13:26:16 -0700

    Dragon Fight

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,17 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int playerScore =0; 
 		// 2.  Ask the user a question 
-		
+		 String Question =JOptionPane.showInputDialog(null,"You know who 16th president is?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (Question.equalsIgnoreCase(" ")
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
